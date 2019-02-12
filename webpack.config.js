19384444dd76344a1b2de61173448dd9e719ac0f
Darknov <darknov.dono@gmail.com>
2019-02-12 19:40:39 +0100

    Add webpack config and babel env preset

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  entry: "./js/main.js",
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
+  }
+};
