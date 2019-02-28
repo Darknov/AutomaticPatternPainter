fc536575b1e0a79f1dd1eca814f849ab81957902
Darknov <darknov.dono@gmail.com>
2019-02-28 01:23:48 +0100

    Add drawing functionality in different form

--- js/Limb.js
+++ js/Limb.js
@@ -1,9 +1,65 @@
 export default class Limb {
-    constructor() {
+    constructor(parent = startingLimb) {
+      this.id = ++Limb.amount;
+      this.parent = parent;
+      console.log(this.parent);
+      this.position = {
+        x: 0 + parent.position.x,
+        y: 0 + parent.position.y
+      }
+      
+      this.length = 50;
+      this.angularVelocity = 0.01;
+      this.angle = 0;
+      this.startingAngle = 50;
+      this.color = "blue";
+      this.size = 0.5;
+      this.isDrawing = true;
+      this.toDelete = false;
+    }
+
+    update(p5) {
+      const relativePosition = {x: this.length * Math.cos(this.getAngle()), y: this.length * Math.sin(this.getAngle())}
+      
+      this.position.x = this.parent.position.x + relativePosition.x;
+      this.position.y = this.parent.position.y + relativePosition.y;
+
+      this.angle += this.angularVelocity;
+    }
+
+    draw(p5, background) {
+      p5.image(background, 0, 0);
+
+      p5.line(this.parent.position.x, this.parent.position.y, this.position.x, this.position.y);
+      p5.ellipse(this.position.x, this.position.y, 5, 5);
+      if(this.isDrawing) {
+        background.stroke(this.color);
+        background.fill(this.color);
+        background.circle(this.position.x, this.position.y, this.size);
+      }
+
 
     }
 
-    draw(p5) {
-
+    getAngle() {
+      return this.angle + this.startingAngle + this.parent.getAngle();
     }
 }
+
+Limb.amount = 0;
+
+
+
+const startingLimb = {
+  position: {
+    x: 300,
+    y: 300
+  },
+  length: 0,
+  angularVelocity: 0,
+  angle: 0,
+  getAngle: function() {
+    return this.angle;
+  }
+}
+
--- js/main.js
+++ js/main.js
@@ -1,20 +1,54 @@
-import Limb from "./Limb";
+import Limb from "./Limb.js";
 
-const setup = p5 => {
+const limbs = [];
+let currentLimb;
+let backgroundCanvas;
+let speed = 10;
+
+const setup = (p5) => {
   p5.createCanvas(600, 600);
+  backgroundCanvas = p5.createGraphics(600,600);
   p5.background(255);
-};
+  let test1 = new Limb();
+  currentLimb = test1;
+  let test2 = new Limb(test1);
+  let test3 = new Limb(test2);
+  let test4 = new Limb(test3);
+  let test5 = new Limb(test4);
+  test1.color = "black";
+  test2.color = "red";
+  test3.color = "green";
+  test4.color = "yellow";
+  test5.angularVelocity = -0.03;
+  limbs.push(test1, test2, test3, test4, test5);
+}
 
-const draw = p5 => {
+const draw = (p5, background) => {
+
+  for(let i = 0; i < speed; i++)
+  {
+    p5.background(255);
+    p5.image(background, 0, 0);
+
+    
+  
+    limbs.forEach(limb => {
+      limb.update(p5);
+    });
+  
+    limbs.forEach(limb => {
+      limb.draw(p5, background);
+    });
+  }
 
 };
 
 new p5(p5 => {
   p5.setup = () => {
-    setup(p5);
+    setup(p5, backgroundCanvas);
   };
 
   p5.draw = () => {
-    draw(p5);
+    draw(p5, backgroundCanvas);
   };
 });
